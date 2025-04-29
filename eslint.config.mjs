e59bede94e2fb861d595e430793cdcc7c923f19f
Suwan Aryal <aryalsuwan.2003@gmail.com>
2025-04-28 19:25:11 -0500

    eslint disabled

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  // Add a new configuration object to define custom rules
+  {
+    rules: {
+      // Disable the 'quotes' rule to allow both single and double quotes
+      'quotes': 'off',
+      // You can add other custom rules here if needed
+    },
+  },
+];
 
 export default eslintConfig;
